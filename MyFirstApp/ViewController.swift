3e89c7ee73635f618fee8d5142bc5ce6e646c077
John Connor <90540757+Xmoon1@users.noreply.github.com>
2023-03-20 15:12:13 +0700

    first ios app

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textWelcomeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func buttonToChange() {
+        textWelcomeLabel.text = "Welcome to My First App"
+    }
+    
 }
 
